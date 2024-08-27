7520f97af13216ec2c18f5882e3b9ae6b1009bd9
kakakjihan <jihansahfitri6@gmail.com>
2024-08-27 21:45:34 +0700

    menampilkan nilai dari variabel php

---
+++ 9 mencetak dengan echo.php
@@ -0,0 +1,7 @@
+<?php
+// membuat variabel baru
+$nama_barang = "Minyak wangi";
+$harga = 20000;
+$tempat = "kuala simpang";
+// menampilkan isi variabel
+echo "jihan membeli $nama_barang seharga Rp $harga di$tempat";
