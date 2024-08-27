fb6dfd03e09a6a362b8fd32df18cd6236c854833
kakakjihan <jihansahfitri6@gmail.com>
2024-08-27 13:04:41 +0700

    membuat komentar di php

---
+++ 6 penulisan komentar di php.php
@@ -0,0 +1,18 @@
+<?php 
+
+// ini adalah komentar
+echo "Hello world";
+
+/*
+ini adalah komentar 
+yang lebih dari satu 
+baris
+*/
+echo "<br>saya sudah hampir dewasa";
+echo "<br>saya sudah minum teh";
+echo "<br>by: jihan";
+
+
+
+?>
+
