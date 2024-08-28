93d4d4c58f0e5e22714dd0afb52872a46c15afe1
kakakjihan <jihansahfitri6@gmail.com>
2024-08-28 08:43:22 +0700

    membuat tipe data di php

---
+++ 15 tipe data pada php.php
@@ -0,0 +1,23 @@
+<?php
+// tipe data char (karakter)
+$jenis_kelamin = 'p';
+
+// tipe data string (teks)
+$nama_lengkap = "jihansyafitri";
+
+// tipe data integer
+$umur = 15;
+// tipe data float
+$berat = 50; 
+// tipe data float
+$tinggi = 160;
+
+// tipe data boolean
+$pelajar = false;
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "pelajar: $pelajar";
+?>
